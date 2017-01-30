d1ffaada5568eec16f2b816439db3028b4fea284
priyankabp <pbphapale@gmail.com>
2017-01-30 18:57:31 -0500

    Add code to change text with second button.

--- app/src/main/java/com/example/android/csit551_w2_demo1/MainActivity.java
+++ app/src/main/java/com/example/android/csit551_w2_demo1/MainActivity.java
@@ -6,16 +6,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.example.android.csit551_w2_demo1.R.id.textView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView textView1;
+    private TextView textView2;
     private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.tv_1_act1);
+        textView1 = (TextView) findViewById(R.id.tv_1_act1);
+        textView2 = (TextView) findViewById(R.id.tv_2_act2);
         findViewById(R.id.btn_act1).setOnClickListener(new MyLsnr());
         /*button=(Button)findViewById(R.id.btn_act1);
         button.setOnClickListener(new MyLsnr());*/
@@ -27,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
+    public void mOnClick(View view) {
+        textView2.setText("Clicked");
+    }
+
     class MyLsnr implements View.OnClickListener{
 
         @Override
         public  void onClick(View view){
-            textView.setText("New Text");
+            textView1.setText("New Text");
         }
     }
 
