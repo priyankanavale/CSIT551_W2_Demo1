35f9cdfc60203f0d2f65083d6369fefac63bce04
priyankabp <pbphapale@gmail.com>
2017-01-30 19:22:18 -0500

    Add navigation from activity1 to activity2 and back to activity1.

--- app/src/main/java/com/example/android/csit551_w2_demo1/MainActivity.java
+++ app/src/main/java/com/example/android/csit551_w2_demo1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.csit551_w2_demo1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView1 = (TextView) findViewById(R.id.tv_1_act1);
         textView2 = (TextView) findViewById(R.id.tv_2_act2);
+
         findViewById(R.id.btn1_act1).setOnClickListener(new MyLsnr());
         findViewById(R.id.btn2_act2).setOnClickListener(new MyLsnr());
         /*button=(Button)findViewById(R.id.btn_act1);
@@ -32,20 +34,17 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
-    public void mOnClick(View view) {
-        textView2.setText("Second Text");
-    }
-
     class MyLsnr implements View.OnClickListener{
 
         @Override
         public  void onClick(View view){
-            if(view.getId()==R.id.btn1_act1)
+            if(view.getId()==R.id.btn1_act1) {
                 textView1.setText(getString(R.string.label1));
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
             else if(view.getId()==R.id.btn2_act2)
                 textView2.setText("New Text 2");
         }
     }
-
-
 }
